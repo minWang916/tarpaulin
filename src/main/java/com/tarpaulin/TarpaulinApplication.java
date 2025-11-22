1921b9256554cf3a3c483de4ca9845b6d6b0e515
Do Minh Quang <116493016+minWang916@users.noreply.github.com>
2025-11-22 16:43:21 +0700

    init PR

---
+++ src/main/java/com/tarpaulin/TarpaulinApplication.java
@@ -0,0 +1,13 @@
+package com.tarpaulin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarpaulinApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TarpaulinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarpaulin/TarpaulinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarpaulin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarpaulinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
